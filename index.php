d832ac079c0173b8981cb573dec079785ef59de1
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-10-27 16:09:55 +0200

    [add] layout index form

--- index.php
+++ index.php
@@ -8,11 +8,32 @@
 
   <title>PHP Strong Password Generator</title>
 </head>
-<body>
-  <div class="container">
-    <h1>Strong Password Generator</h1>
-    <h2>Genera una password sicura</h2>
-    <p>Scegli quanti caratteri deve avere la password (minimo 8 caratteri, massimo 32 caratteri)</p>
+<body class="vh-100 bg-black">
+  <div class="container h-100 d-flex justify-content-center align-items-center ">
+    <!-- <div class="d-flex flex-column align-items-center"> -->
+    <div class="row">
+      <div class="col-12 d-flex flex-column align-items-center">
+        
+        <h1 class="text-secondary fw-bold">Strong Password Generator</h1>
+        <h2 class="text-white fw-bold">Genera una password sicura</h2>
+        <p class="bg-info-subtle w-100 py-3 px-5">Scegli quanti caratteri deve avere la password (min 8 - max 32 caratteri)</p>
+  
+        <form action="index.php" method="POST" class="bg-light w-100">
+          <div class="row">
+            <div class="col-12 d-flex justify-content-center mt-4">
+              <label for="quantity">Lunghezza Password:</label>
+              <input class="w-25 ms-3" type="number" id="quantity" name="quantity" min="8" max="32">
+            </div>
+            <div class="col-12 d-flex justify-content-center my-4">
+              <button type="submit" class="btn btn-primary">Invia</button>
+              <button type="reset" class="btn btn-secondary ms-3">Annulla</button>
+
+            </div>
+          </div>
+        </form>
+
+      </div>  
+    </div>
   </div>
   
 </body>
