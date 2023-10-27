7a02778ea1073fd1d8335cb40eac431de86ff871
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-10-27 15:35:30 +0200

    [add] readme

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css' integrity='sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==' crossorigin='anonymous'/>
+
+  <title>PHP Strong Password Generator</title>
+</head>
+<body>
+  <div class="container">
+    <h1>Strong Password Generator</h1>
+    <h2>Genera una password sicura</h2>
+    <p>Scegli quanti caratteri deve avere la password (minimo 8 caratteri, massimo 32 caratteri)</p>
+  </div>
+  
+</body>
+</html>
